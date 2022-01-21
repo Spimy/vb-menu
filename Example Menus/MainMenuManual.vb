e2969c14e9266b0227387a4808d610ba88e6a46a
William Law <williamlaw_33@hotmail.com>
2022-01-21 15:19:53 +0400

    Add example menu for manual handle selection

---
+++ Example Menus/MainMenuManual.vb
@@ -0,0 +1,42 @@
+﻿Public Class MainMenuManual : Inherits BaseMenu
+
+    Sub New()
+        MyBase.New({
+            "1. Add 2 numbers",
+            "2. Subtract 2 numbers",
+            "3. Exit"
+        })
+    End Sub
+
+    Function InputNums() As Integer()
+        Console.Write("First number: ")
+        Dim Num1 As Integer = Console.ReadLine()
+
+        Console.Write("Second number: ")
+        Dim Num2 As Integer = Console.ReadLine
+
+        Return {Num1, Num2}
+    End Function
+
+    Overloads Sub SelectOption()
+        Dim MenuOption = MyBase.SelectOption()
+
+        Select Case MenuOption
+            Case 1
+                Dim Nums As Integer() = InputNums()
+                Console.WriteLine("Result of {0} + {1} = {2}", Nums(0), Nums(1), Nums(0) + Nums(1))
+            Case 2
+                Dim Nums As Integer() = InputNums()
+                Console.WriteLine("Result of {0} - {1} = {2}", Nums(0), Nums(1), Nums(0) - Nums(1))
+            Case 3 : Environment.Exit(0)
+        End Select
+
+        Console.WriteLine()
+        Console.WriteLine("Press any key to continue...")
+        Console.ReadKey()
+
+        Me.Display()
+        Me.SelectOption()
+    End Sub
+
+End Class
