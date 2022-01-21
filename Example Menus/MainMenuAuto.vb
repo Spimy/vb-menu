64a11f9f1117904d43e93b7c85854a81365e9859
William Law <williamlaw_33@hotmail.com>
2022-01-21 15:19:46 +0400

    Add example menu for auto handle selection

---
+++ Example Menus/MainMenuAuto.vb
@@ -0,0 +1,35 @@
+﻿Public Class MainMenuAuto : Inherits BaseMenu
+
+    Sub New()
+        MyBase.New({
+            "Add 2 numbers",
+            "Subtract 2 numbers",
+            "Exit"
+        }, True)
+    End Sub
+
+    Function InputNums() As Integer()
+        Console.Write("First number: ")
+        Dim Num1 As Integer = Console.ReadLine
+
+        Console.Write("Second number: ")
+        Dim Num2 As Integer = Console.ReadLine
+
+        Return {Num1, Num2}
+    End Function
+
+    Sub Option1()
+        Dim Nums As Integer() = InputNums()
+        Console.WriteLine("Result of {0} + {1} = {2}", Nums(0), Nums(1), Nums(0) + Nums(1))
+    End Sub
+
+    Sub Option2()
+        Dim Nums As Integer() = InputNums()
+        Console.WriteLine("Result of {0} - {1} = {2}", Nums(0), Nums(1), Nums(0) - Nums(1))
+    End Sub
+
+    Sub Option3()
+        Environment.Exit(0)
+    End Sub
+
+End Class
