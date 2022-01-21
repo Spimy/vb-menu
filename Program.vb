65aa7f44278edbbadf3d81f99c35e506726bdc33
William Law <williamlaw_33@hotmail.com>
2022-01-21 15:20:15 +0400

    Use example menu

--- Program.vb
+++ Program.vb
@@ -2,6 +2,14 @@ Imports System
 
 Module Program
     Sub Main(args As String())
-        Console.WriteLine("Hello World!")
+        With New MainMenuAuto()
+            .Display()
+            .SelectOption(True)
+        End With
+
+        'With New MainMenuManual()
+        '    .Display()
+        '    .SelectOption()
+        'End With
     End Sub
 End Module
